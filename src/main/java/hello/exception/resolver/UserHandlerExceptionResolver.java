6557b0d9890c560cb41091eaa430e9f4277b4d0f
Junsung <031wnstjd@naver.com>
2023-10-23 00:02:36 +0900

    HandlerExceptionResolver 활용

---
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -0,0 +1,53 @@
+package hello.exception.resolver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.exception.exception.UserException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        try {
+
+            if (ex instanceof UserException) {
+                log.info("UserException resolver to 400");
+                String acceptHeader = request.getHeader("accept");
+                // status 정보만 response 객체에 담아줄 뿐, UserException 자체는 여기서 모두 처리 됨 (더이상 예외 전파가 안 됨)
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+                if ("application/json".equals(acceptHeader)) {
+                    Map<String, Object> errorResult = new HashMap<>();
+                    errorResult.put("ex", ex.getClass());
+                    errorResult.put("message", ex.getMessage());
+
+                    String result = objectMapper.writeValueAsString(errorResult);
+
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("utf-8");
+                    response.getWriter().write(result);
+                    return new ModelAndView(); // 예외를 먹어버리고 정상 흐름으로 리턴 (WAS 입장에서는 정상 처리 된 것으로 인식)
+                } else {
+                    // text/html
+                    return new ModelAndView("error/500");
+                }
+            }
+
+        } catch (IOException e) {
+            log.error("resolver ex", e);
+        }
+
+        return null;
+    }
+}
